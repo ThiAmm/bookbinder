c8a9c7a6ccd1b70545f2486e4da8f17e59c1f344
Thilo Ammon <thilo.ammon@msg.group>
2021-09-28 09:26:37 +0200

    Bind books with signatures with fixed page number

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
-FROM ubuntu:14.04
+FROM ubuntu:20.04
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt-get update -y && apt-get install -y \
+  texlive-extra-utils \
+  texlive-latex-extra \
+  texlive-latex-recommended \
+  texlive texlive-lang-german
+RUN apt-get -y install poppler-utils
+RUN apt-get -y install qpdf
 WORKDIR /bookbinder
 COPY bookbinder.sh /
 ENTRYPOINT ["/bookbinder.sh"]
--- bookbinder.sh
+++ bookbinder.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+cd $1
 for filename in $1/*.pdf; 
 do
-  mv $filename "${filename/.pdf/_bookbinded.pdf}"
+  mkdir tmp
+  numberofpages=$(pdfinfo $filename | awk '/^Pages:/ {print $2}')
+  for i in `seq 1 8 $numberofpages`
+  do
+      num=$((i + 7))
+      if [ "$num" -lt "$numberofpages" ]; then
+        j=$num;
+      else
+	j=$numberofpages;
+      fi
+      qpdf $filename --pages . $i-$j -- tmp/output_$i-$j.pdf
+      pdfbook2 tmp/output_$i-$j.pdf
+      rm tmp/output_$i-$j.pdf
+  done
+  pdfunite tmp/output*.pdf $filename-output.pdf
+  rm -rf tmp
 done
+
