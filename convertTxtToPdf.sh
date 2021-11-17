e41c23739ecd6a030b7f6be4640818db7ca7eeec
Thilo Ammon <thilo.ammon@msg.group>
2021-11-17 14:22:46 +0100

    New lines in txt-file are new lines in pdf

--- convertTxtToPdf.sh
+++ convertTxtToPdf.sh
@@ -1,6 +1,6 @@
 echo "Convert all txt-files in $1 to pdf"
 for filename in $1/*.txt;
 do
-	pandoc $filename -o "${filename%%.*}".pdf;
+	pandoc $filename -f markdown+hard_line_breaks -o "${filename%%.*}".pdf;
 done
 
